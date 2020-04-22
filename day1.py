ad4e44d22fd5fbeb5de3835a4fed5651ba4ec468
Fortune Adekogbe <53936357+Fortune-Adekogbe@users.noreply.github.com>
2020-04-22 05:41:25 +0100

    Update

--- day1.py
+++ day1.py
@@ -1,5 +1,4 @@
 
 def twofer(*name):
-    # Enter your code here. Read input from STDIN. Print output to STDOUT
     a= list(name)[0] if len(name)>0 else 'you'
     return f'One for {a}, one for me.'
